5f6386c2b7b371e7a55f83a4ae5c03278ce0cf60
priyanka02047 <pari828385@gmail.com>
2024-04-07 01:39:05 +0530

    added swiggy type web page by manual data

---
+++ data.js
@@ -0,0 +1,466 @@
+export const restaurantData = [
+  {
+    info: {
+      id: "825670",
+      name: "Captain Sam's Pizza",
+      cloudinaryImageId: "cf6280d32aefb59cc849ef81b39fd3fd",
+      locality: "Model Town",
+      areaName: "Sector 32",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Pizzas",
+        "Desserts",
+        "Beverages",
+        "Pastas",
+        "Fast Food",
+        "Snacks",
+        "Italian",
+      ],
+      avgRating: 4.2,
+      parentId: "382786",
+      avgRatingString: "4.2",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 46,
+        lastMileTravel: 13.7,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "13.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 04:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "EVERY ITEM",
+        subHeader: "@ ₹199",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      isNewlyOnboarded: true,
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-378dd1d8-bff2-42c2-bb94-6b49b56af987",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/captain-sams-pizza-model-town-sector-32-ludhiana-825670",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "415896",
+      name: "Paratha junction",
+      cloudinaryImageId: "vv4iatphke8s4jeahbsu",
+      locality: "Chaudhary Colony",
+      areaName: "Samrala Chowk",
+      costForTwo: "₹200 for two",
+      cuisines: ["Tandoor", "Chinese", "Snacks"],
+      avgRating: 3.9,
+      veg: true,
+      parentId: "3467",
+      avgRatingString: "3.9",
+      totalRatingsString: "50+",
+      sla: {
+        deliveryTime: 56,
+        lastMileTravel: 14.9,
+        serviceability: "SERVICEABLE",
+        slaString: "55-60 mins",
+        lastMileTravelString: "14.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 04:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹50",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-378dd1d8-bff2-42c2-bb94-6b49b56af987",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/paratha-junction-chaudhary-colony-samrala-chowk-ludhiana-415896",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "66711",
+      name: "Shakti Om Vegetarian Dhaba",
+      cloudinaryImageId: "6a0ffb9be6fed79182c886cda4eb6711",
+      locality: "Shivajinagar",
+      areaName: "Samrala Chowk",
+      costForTwo: "₹250 for two",
+      cuisines: ["North Indian", "Desserts"],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "181327",
+      avgRatingString: "4.2",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 44,
+        lastMileTravel: 14.4,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "14.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 02:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "65% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-378dd1d8-bff2-42c2-bb94-6b49b56af987",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/shakti-om-vegetarian-dhaba-shivajinagar-samrala-chowk-ludhiana-66711",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "88435",
+      name: "Shakti Om Vegetarian Dhaba(Shingar Cinema Road)",
+      cloudinaryImageId: "rjsskcfcjvgerftwanzp",
+      locality: "Shivajinagar",
+      areaName: "Samrala Chowk",
+      costForTwo: "₹300 for two",
+      cuisines: ["North Indian", "Desserts"],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "181328",
+      avgRatingString: "4.2",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 47,
+        lastMileTravel: 14.5,
+        serviceability: "SERVICEABLE",
+        slaString: "45-50 mins",
+        lastMileTravelString: "14.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 02:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "65% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-378dd1d8-bff2-42c2-bb94-6b49b56af987",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/shakti-om-vegetarian-dhaba-shingar-cinema-road-shivajinagar-samrala-chowk-ludhiana-88435",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "246661",
+      name: "Pandit Ji Di Hatti",
+      cloudinaryImageId: "zpobylf3euinjdua1mzm",
+      locality: "Ludhiana Junction",
+      areaName: "Ludhiana Junction",
+      costForTwo: "₹200 for two",
+      cuisines: ["Indian", "Continental"],
+      avgRating: 4.4,
+      parentId: "154452",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 42,
+        lastMileTravel: 14.1,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "14.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-13 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-378dd1d8-bff2-42c2-bb94-6b49b56af987",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pandit-ji-di-hatti-ludhiana-junction-ludhiana-246661",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "545766",
+      name: "Kulfiwala",
+      cloudinaryImageId: "ihzmshwnxqzdk0lesuto",
+      locality: "Kashmir Nagar",
+      areaName: "Chaura Bazaar",
+      costForTwo: "₹200 for two",
+      cuisines: ["Ice Cream", "Desserts", "Fast Food", "Pizzas", "Burgers"],
+      avgRating: 4.2,
+      veg: true,
+      parentId: "121676",
+      avgRatingString: "4.2",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 43,
+        lastMileTravel: 14,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "14.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 01:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹125 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-378dd1d8-bff2-42c2-bb94-6b49b56af987",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kulfiwala-kashmir-nagar-chaura-bazaar-ludhiana-545766",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
