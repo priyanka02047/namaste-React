7f6269cfa8c555c28fdb6f1361072b5f9d68ae2a
priyanka02047 <pari828385@gmail.com>
2024-04-14 01:08:08 +0530

    to avoid reload on url change we are using Link component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { Header } from "./components/Header";
+import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import AboutUs from "./components/About";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
-export const Header = () => (
-  <div className="header">
-    <div className="logo">
-      <img src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"></img>
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"></img>
+      </div>
+      <div className="nav-menu">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>Offers</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-    <div className="nav-menu">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Offers</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
+export default Header;
