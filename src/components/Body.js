ee47c783613534675bc0607642efb11fc8cd3289
priyanka02047 <pari828385@gmail.com>
2024-04-13 23:37:47 +0530

    added shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,51 @@
 import { RestaurantCard } from "./RestaurantCard";
 import { restaurantData } from "../utils/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const Body = () => {
-  const [data, setData] = useState(restaurantData);
-  return (
+  const [data, setData] = useState();
+  const [searchtext, setSearchText] = useState("");
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  const fetchAPI = () => {
+    setTimeout(() => {
+      setData(restaurantData);
+    }, 4000);
+    //Url is not working because of cors
+    // const url =
+    //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.00480&lng=75.94630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    // const data = await fetch(url);
+    // const response = await data.json();
+    // console.log(response);
+    // setData(
+    //   response.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    // );
+  };
+  const filtersData =
+    data !== undefined
+      ? data.filter((item) =>
+          item.info.name
+            .toLocaleLowerCase()
+            .includes(searchtext.toLocaleLowerCase())
+        )
+      : [];
+
+  return filtersData === undefined ? (
+    <Shimmer />
+  ) : (
     <div className="body-component">
       <div className="search">
-        <input type="text" placeholder="Search"></input>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchtext}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
         <button type="submit">Search</button>
         <button
           type="submit"
@@ -22,7 +61,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {data.map((item) => (
+        {filtersData.map((item) => (
           <RestaurantCard key={item.info.id} restaurant={item} />
         ))}
       </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,18 @@
+const Shimmer = () => (
+  <div>
+    <div className="Shimmer-Body">
+      <div className="Shimmer-search"></div>
+      <div className="Shimmer-res-container">
+        <div className="Shimmer-cart"></div>
+        <div className="Shimmer-cart"></div>
+        <div className="Shimmer-cart"></div>
+        <div className="Shimmer-cart"></div>
+        <div className="Shimmer-cart"></div>
+        <div className="Shimmer-cart"></div>
+        <div className="Shimmer-cart"></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Shimmer;
